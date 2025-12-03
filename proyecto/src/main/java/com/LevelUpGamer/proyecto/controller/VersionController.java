b261c3e50ed1f317701d57c01b897a0345cb1a57
Fabian <fa.armijog@duocuc.cl>
2025-12-02 22:59:41 -0300

    actualizacion app automatica

---
+++ proyecto/src/main/java/com/LevelUpGamer/proyecto/controller/VersionController.java
@@ -0,0 +1,21 @@
+package com.LevelUpGamer.proyecto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/version")
+public class VersionController {
+
+    @GetMapping("/latest")
+    public Map<String, Object> getLatestVersion() {
+        // Cada vez que subas una app nueva, cambia este número manualmente aquí
+        return Map.of(
+                "versionCode", 2,
+                "url", "/app-release.apk" // Spring Boot sirve static files en la raíz por defecto
+        );
+    }
+}
